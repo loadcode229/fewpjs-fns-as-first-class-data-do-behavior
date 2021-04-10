3769c95fe098c5edfaff9c3f101a4fce4146b984
loadcode229 <sepone229@gmail.com>
2021-04-10 12:43:54 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let current_time = time.split(":");
+  let another_time = current_time[0].concat(current_time[1]);
+  let factored_time = parseInt(another_time, 10);
+  if (factored_time < 1200) {
+    return "Good Morning";
+  } 
+  else if (factored_time > 1700) {
+    return "Good Evening";
+  } 
+  else {
+    return "Good Afternoon";
+  }
+};
+
 /* Write your implementation of displayMessage() */
+function displayMessage() {
+  document.getElementById('greeting').innerText = 'TEST';
+};
